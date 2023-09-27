43468b4e7d3ea2c9d39c5a70216a2ecf67a2bf9c
armsves <armsves@gmail.com>
2023-09-27 19:59:54 +0300

    working test

--- src/components/SearchFlight.tsx
+++ src/components/SearchFlight.tsx
@@ -7,6 +7,7 @@ import { Slider } from '@mui/material';
 
 
 
+
 export function SearchFlight() {
   const { sender, connected } = useTonConnect();
 
@@ -14,7 +15,7 @@ export function SearchFlight() {
   const [cityDeparture, setCityDeparture] = useState("");
   const [city1, setCity1] = useState("");
   const [city2, setCity2] = useState("");
-  const [cityDays, setCityDays] = useState("1");
+  const [cityDays, setcityDays] = useState("1");
   const [adults, setAdults] = useState("1");
 
   const [sliderValue, setSliderValue] = useState(1);
@@ -32,16 +33,171 @@ export function SearchFlight() {
   };
 
   const value = 0;
-
   
   const handleSliderChange = () => {
     setSliderValue(value);
   };
 
+  //**************** */
+
+  async function delay(ms: number): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  async function displayData(cityDeparture: string, city1: string, city2: string, cityDays: string, departureDate: string, adults: string) {
+    try {
+      const loadingAnimation = document.getElementById('loadingAnimation');
+      //loadingAnimation.style.display = 'block';
+      let bestFlight = [];
+      let bestFlight2 = [];
+  
+      for (let i = 0; i < 10; i++) {
+        try {
+          bestFlight = await buscarVuelo(cityDeparture, city1, city2, cityDays, departureDate, adults);
+          bestFlight2 = await buscarVuelo(cityDeparture, city2, city1, cityDays, departureDate, adults);
+          break; 
+        } catch (error) {
+          console.error(`Error fetching flight data. Retrying... (${i+1}/10)`);
+          await delay(1000); 
+        }
+      }
+  
+      if (bestFlight[0] > bestFlight2[0]) {
+        bestFlight = bestFlight2;
+      }
+  
+      const link = document.getElementById("link");
+      const precio = document.getElementById("precio");
+      //const agents = document.getElementById("agents");
+      //precio.innerHTML = bestFlight[0] / 1000;
+      //agents.innerHTML = bestFlight[1];
+      //link.setAttribute("href", bestFlight[2]);
+      //loadingAnimation.style.display = 'none';
+      //const results = document.getElementById('results');
+      //results.style.display = 'block';
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  async function buscarVuelo(cityDeparture: string, city1: string, city2: string, cityDays: string, departureDate: string, adults: string) {
+    let flightData = [];
+    const url = "https://corsproxy.armsves.workers.dev/corsproxy/";
+  
+    console.log(departureDate)
+    const departure = departureDate.split("-");
+    let date = new Date(departure[0], departure[1], departure[2]);
+    console.log(departure[0])
+    console.log(date)
+  
+    cityDays = parseInt(cityDays)
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    console.log(date)
+    date.setDate(day + cityDays)
+    const day2 = date.getDate();
+    const month2 = date.getMonth() + 1;
+    const year2 = date.getFullYear();
+    console.log(date)
+    date.setDate(day2 + cityDays)
+    const day3 = date.getDate();
+    const month3 = date.getMonth() + 1;
+    const year3 = date.getFullYear();
+    console.log(day3)
+
+  
+    const headers = {};
+    const data = {
+      "query": {
+        "market": "US",
+        "locale": "en-US",
+        "currency": "EUR",
+        "query_legs": [
+          {
+            "origin_place_id": { "iata": cityDeparture },
+            "destination_place_id": { "iata": city1 },
+            "date": { "year": year, "month": month, "day": day }
+          },
+          {
+            "origin_place_id": { "iata": city1 },
+            "destination_place_id": { "iata": city2 },
+            "date": { "year": year2, "month": month2, "day": day2 }
+          },
+          {
+            "origin_place_id": { "iata": city2 },
+            "destination_place_id": { "iata": cityDeparture },
+            "date": { "year": year3, "month": month3, "day": day3 }
+          }
+        ],
+        "adults": 1,
+        "cabin_class": "CABIN_CLASS_ECONOMY",
+        "nearbyAirports": false
+      }
+    };
+    console.log(data)
+  
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(data)
+      });
+  
+      if (response.ok) {
+        const vuelosFinal = await response.json();
+
+        
+        const jsonObject = vuelosFinal['content']['results']['itineraries'];
+        const jsonArray = Object.values(jsonObject);
+  
+        jsonArray.sort((a, b) => {
+          console.log(a["pricingOptions"][0]["price"]["amount"] + " /n")
+          const amountA = parseInt(a["pricingOptions"][0]["price"]["amount"]);
+          const amountB = parseInt(b["pricingOptions"][0]["price"]["amount"]);
+          if (amountA < amountB) { return -1; } else if (amountA > amountB) { return 1; } else { return 0; }
+        });
+        flightData = [jsonArray[0]["pricingOptions"][0]["price"]["amount"]/1000, jsonArray[0]["pricingOptions"][0]["agentIds"][0], jsonArray[0]["pricingOptions"][0]["items"][0]["deepLink"]];
+        if (flightData != null) { setResponses(flightData[0]); }
+        setResponsesLink(flightData[2]);
+        return flightData;
+      } else {
+        console.log('Request failed with status code:', response.status);
+        throw new Error('API request failed');
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+      throw error;
+    }
+  }
+
+  //***************** */
+  const [responses, setResponses] = useState(null);
+  const [responsesLink, setResponsesLink] = useState(null);
+
+  async function fetchData() {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/ability/3");
+      if (response.ok) {
+        const data = await response.json();
+        setResponses(data);
+      } else {
+        console.error('API request failed with status code:', response.status);
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  }
+
+  //**********
+
+  //
 //<Input style={{ marginRight: 8 }} type="number" value={tonAmount} onChange={(e) => setTonAmount(e.target.value)}></Input>
 //<Input type="number" value={cityDeparture} onChange={(e) => setCityDeparture(e.target.value)} >
 //<Slider aria-label="Columns" value={sliderValue} onChange={handleSliderChange} min={1} max={6} step={1} />
-//<Input type="number" min="1" value={cityDays} onChange={(e) => setCityDays(e.target.value)} ></Input>
+//<Input type="number" min="1" value={cityDays} onChange={(e) => setcityDays(e.target.value)} ></Input>
 //<Input value={cityDeparture} onChange={(e) => setCityDeparture(e.target.value)} ></Input>
 //  <Slider aria-label="Columns" value={sliderValue} onChange={handleSliderChange} min={1} max={6} step={1} />
   return (
@@ -65,7 +221,7 @@ export function SearchFlight() {
 
         <FlexBoxRow>
           <label>Days in each city: </label>
-          <Input type="number" min="1" value={cityDays} onChange={(e) => setCityDays(e.target.value)} ></Input>
+          <Input type="number" min="1" value={cityDays} onChange={(e) => setcityDays(e.target.value)} ></Input>
         </FlexBoxRow>
 
         <FlexBoxRow>
@@ -79,14 +235,23 @@ export function SearchFlight() {
           
         </FlexBoxRow>
 
-        <Button disabled={!connected} style={{ marginTop: 18 }}
+        <Button style={{ marginTop: 18 }}
           onClick={async () => {
-            sender.send({
-              to: Address.parse(tonRecipient),
-              value: toNano(tonAmount),
-            });
+            buscarVuelo(cityDeparture, city1, city2, cityDays, departureDate, adults);
           }}
         >Search</Button>
+      </FlexBoxCol>
+      <FlexBoxCol>
+      <h3>Results</h3>
+      {responses ? (
+        <div>
+          <h1>The Best price is: </h1>
+          <pre>{JSON.stringify(responses, null, 2)}</pre>
+          <a href={responsesLink} target="_blank">Buy Tickets</a>
+        </div>
+      ) : (
+       <></>
+      )}
       </FlexBoxCol>
     </Card>
   );
