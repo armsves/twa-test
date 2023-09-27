2791637cfe47a48676bc24e26215fb796a80e16f
armsves <armsves@gmail.com>
2023-09-27 15:25:37 +0300

    test

--- src/components/SearchFlight.tsx
+++ src/components/SearchFlight.tsx
@@ -3,11 +3,9 @@ import styled from "styled-components";
 import { Address, toNano } from "ton";
 import { useTonConnect } from "../hooks/useTonConnect";
 import { Card, FlexBoxCol, FlexBoxRow, Button, Input } from "./styled/styled";
-//import { Slider } from '@mui/material';
+import { Slider } from '@mui/material';
 
-const Slider = ({ value, onChange }) => (
-    <input type="range" min="1" max="10" step="1" value={value} onChange={onChange} />
-  );
+
 
 export function SearchFlight() {
   const { sender, connected } = useTonConnect();
@@ -19,11 +17,11 @@ export function SearchFlight() {
   const [cityDays, setCityDays] = useState("1");
   const [adults, setAdults] = useState("1");
 
-  const [sliderValue, setSliderValue] = useState(6);
+  const [sliderValue, setSliderValue] = useState(1);
 
   const [departureDate, setDepartureDate] = useState("");
 
-  const [tonRecipient, setTonRecipient] = useState("EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c");
+  const [tonRecipient, setTonRecipient] = useState("EQAA");
 
   const getCurrentDate = (): string => {
     const today = new Date();
@@ -33,85 +31,26 @@ export function SearchFlight() {
     return `${year}-${month}-${day}`;
   };
 
+  const value = 0;
 
-  //
-  const [searchTerm, setSearchTerm] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
-  const [selectedSuggestion, setSelectedSuggestion] = useState(null);
-
-  useEffect(() => {
-    if (searchTerm.length > 2) {
-      const url = 'https://corsautosuggest.armsves.workers.dev/corsproxy/';
-      const data = {
-        query: {
-          market: 'UK',
-          locale: 'en-GB',
-          searchTerm: searchTerm,
-          includedEntityTypes: [
-            'PLACE_TYPE_CITY',
-            'PLACE_TYPE_COUNTRY',
-            'PLACE_TYPE_AIRPORT',
-          ],
-        },
-        limit: 10,
-        isDestination: true,
-      };
-
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          const places = data.places;
-          setSuggestions(places);
-        })
-        .catch((error) => console.error('Error:', error));
-    } else {
-      setSuggestions([]); // Clear suggestions when input length is less than 3
-    }
-  }, [searchTerm]);
-
-  const handleInputChange = (value) => {
-    setSearchTerm(value);
-  };
-
-  const handleSuggestionClick = (place) => {
-    setSelectedSuggestion(place);
-    setSearchTerm(`${place.cityName}, ${place.countryName} (${place.iataCode})`);
-  };
-  //
-  /*
-  const handleSliderChange = (event, value) => {
+  
+  const handleSliderChange = () => {
     setSliderValue(value);
   };
-*/
+
 //<Input style={{ marginRight: 8 }} type="number" value={tonAmount} onChange={(e) => setTonAmount(e.target.value)}></Input>
 //<Input type="number" value={cityDeparture} onChange={(e) => setCityDeparture(e.target.value)} >
 //<Slider aria-label="Columns" value={sliderValue} onChange={handleSliderChange} min={1} max={6} step={1} />
 //<Input type="number" min="1" value={cityDays} onChange={(e) => setCityDays(e.target.value)} ></Input>
 //<Input value={cityDeparture} onChange={(e) => setCityDeparture(e.target.value)} ></Input>
+//  <Slider aria-label="Columns" value={sliderValue} onChange={handleSliderChange} min={1} max={6} step={1} />
   return (
     <Card>
       <FlexBoxCol>
         <h3>Search Flight</h3>
         <FlexBoxRow>
           <label>Departure City: </label>
-          <Input value={cityDeparture} onChange={(e) => handleInputChange(e.target.value)} ></Input>
-          <div id="suggestionBox">
-        {suggestions.map((place, index) => (
-          <p
-            key={index}
-            onClick={() => handleSuggestionClick(place)}
-            className={selectedSuggestion === place ? 'selected' : ''}
-          >
-            {`${place.cityName}, ${place.countryName} (${place.iataCode})`}
-          </p>
-        ))}
-      </div>
+          <Input value={cityDeparture} onChange={(e) => setCityDeparture(e.target.value)} ></Input>
         </FlexBoxRow>
 
         <FlexBoxRow>
@@ -125,8 +64,8 @@ export function SearchFlight() {
         </FlexBoxRow>
 
         <FlexBoxRow>
-          <label>Days in each city: </label>{sliderValue}
-          <Slider value={sliderValue} onChange={(e) => setSliderValue(e.target.value)} />
+          <label>Days in each city: </label>
+          <Input type="number" min="1" value={cityDays} onChange={(e) => setCityDays(e.target.value)} ></Input>
         </FlexBoxRow>
 
         <FlexBoxRow>
